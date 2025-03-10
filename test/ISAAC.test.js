4d9841f729b1c957980082e59aa1187c81a74d2a
Davide Renzi <davide.renzi@gmail.com>
2025-03-10 10:19:59 +0100

    Create ISAAC.test.js

---
+++ test/ISAAC.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+const ISAAC = require('../lib/ISAAC');
+
+describe('ISAAC PRNG', function() {
+    let isaac;
+
+    beforeEach(function() {
+        isaac = new ISAAC();
+    });
+
+    it('should generate a 32-bit random integer', function() {
+        const randomInt = isaac.random();
+        assert.ok(Number.isInteger(randomInt));
+    });
+
+    it('should generate a 53-bit random float', function() {
+        // Get the two 32-bit numbers
+        let random1 = isaac.random();
+        let random2 = isaac.random();
+
+        // Combine the numbers to get a 53-bit number
+        // The first number occupies the 21 most significant bits, the second the 32 least significant bits
+        let combined = (random1 * 2 ** 21) + (random2 >>> 11);
+
+        // Divide by 2^53 to get a number between 0 and 1
+        const randomFloat = combined / 2 ** 53;
+        assert.ok(randomFloat >= 0 && randomFloat < 1);
+    });
+});
